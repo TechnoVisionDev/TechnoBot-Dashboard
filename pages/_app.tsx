180c4154a48426e06607285fdb9f75af2b41824f
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-01-15 13:08:01 -0800

    Fix build errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import type { AppProps } from 'next/app'
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
@@ -8,17 +7,7 @@ config.autoAddCss = false;
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet"/>
-        <title>TechnoBot</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />
 }
 
 export default MyApp
