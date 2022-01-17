b37ca196b91205486b626902715ce9b46a902a26
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-01-17 15:51:36 -0800

    Add mongodb backend

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -2,11 +2,13 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 import url from 'url';
 
+import { connectToDatabase } from '../../../lib/mongodb';
+
 async function handler(req : NextApiRequest, res : NextApiResponse) {
     if (req.method === 'GET') {
         // Get application data
         const { code } = req.query;
-        if (!code) { return res.redirect(400, '/error') }
+        if (!code) { return res.redirect(400, '/error'); }
         const authHeaders = { headers : {'Content-Type': 'application/x-www-form-urlencoded'} };
         const payload = buildOAuth2CredentialsRequest(code.toString());
 
@@ -16,6 +18,9 @@ async function handler(req : NextApiRequest, res : NextApiResponse) {
             const { access_token, refresh_token } = response.data;
 
             //TODO: Save access_token and refresh_token in database
+            let { db } = await connectToDatabase();
+            //await db.collection('sessions').insertOne({});
+
             return res.send(`Access Token: ${access_token}`);
         } catch (err) {
             console.log(err);
