5109f9a87f055a4691ca4a8696e84bf68a843545
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-01-19 15:38:33 -0800

    Finalize dropdown menu design

--- components/layout/Profile.tsx
+++ components/layout/Profile.tsx
@@ -1,6 +1,9 @@
 import { signOut } from 'next-auth/react';
+import { useState } from 'react';
+import Link from 'next/link';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown, faServer, faSignOutAlt, faGem } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Profile.module.css';
 
@@ -10,11 +13,37 @@ type Props = {
 };
 
 const Profile = ({ name, avatar }: Props) => {
+    const [isClicked, setClicked] = useState(false);
+
+    const clickHandler = () => {
+        setClicked(!isClicked);
+    }
+
     return (
-        <div className={styles.profile} onClick={() => signOut({callbackUrl: window.location.origin})}>
-            <p>{name}</p>
-            <img className={styles.avatar} src={avatar} />
-            <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+        <div className={styles.dropdown} onClick={clickHandler}>
+            <div className={styles.profile}>
+                <p>{name}</p>
+                <img className={styles.avatar} src={avatar} />
+                <FontAwesomeIcon icon={faCaretDown} />
+            </div>
+            <div className={`${styles.menu} ${!isClicked ? styles.hidden : ''}`}>
+                <Link href="/servers">
+                    <div className={styles.button}>
+                        <FontAwesomeIcon icon={faServer} />
+                        <p>Servers</p>
+                    </div>
+                </Link>
+                <Link href="/premium">
+                    <div className={styles.button}>
+                        <FontAwesomeIcon icon={faGem} />
+                        <p>Premium</p>
+                    </div>
+                </Link>
+                <div className={styles.button} onClick={() => signOut({callbackUrl: window.location.origin})}>
+                    <FontAwesomeIcon icon={faSignOutAlt} />
+                    <p>Sign Out</p>
+                </div>
+           </div>
         </div>
     );
 }
