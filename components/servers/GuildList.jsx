9db969538b4acff45187991283f521a083765507
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-04-15 12:55:00 -0700

    Add loading spinner for server selector

--- components/servers/GuildList.jsx
+++ components/servers/GuildList.jsx
@@ -1,4 +1,5 @@
 import Guild from './Guild';
+import { SpinnerCircular } from 'spinners-react';
 
 import styles from './GuildList.module.css';
 
@@ -6,7 +7,7 @@ function GuildList({data, isLoading}) {
 
     const items = [];
     if (isLoading) {
-      items.push(<p className={styles.loading}>Loading...</p>);
+      items.push(<SpinnerCircular color="#57a5ff" thickness={200} />);
     } else {
       for (var i = 0; i < data['length']; i++) {
         items.push(<Guild key={i} data={data[i]} />);
